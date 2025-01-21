8da1528f87cb260b7d03d148de91e08061d068f6
Ranga McMil <ranga@Rangas-MacBook-Air.local>
2025-01-21 16:04:09 +0200

    refactor features

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -8,8 +8,11 @@ import { Questions } from "@/components/questions";
 import ShimmerButton from "@/components/shimmer-button";
 import { WobbleCardShow } from "@/components/wobble";
 import { WordReveal } from "@/components/word-reveal";
+import { ComponentIcon, Users } from "lucide-react";
+
 
 import { cn } from "@/lib/utils";
+import { FeatureGrid } from "@/components/features";
 
 export const metadata = {
   title: "Home",
@@ -31,7 +34,7 @@ export default async function IndexPage() {
 
               <div className="mt-6">
                 <h1 className="relative mb-6 max-w-4xl text-left text-4xl font-bold dark:text-zinc-100 sm:text-7xl md:text-7xl xl:text-7xl">
-                  Fiscit: Your partner in integrations with FDMS
+                  Fiscit: Your partner in integrations with FDMS.
                 </h1>
               </div>
 
@@ -108,79 +111,76 @@ export default async function IndexPage() {
             Discover the range of services we offer.
           </div>
 
-
-            <div className="flex flex-col items-center gap-4 sm:flex-row sm:justify-center py-4 sm:py-20 md:py-20 xl:py-20">
-                <figure
-                className={cn(
-                "relative w-full max-w-xs cursor-pointer overflow-hidden rounded-2xl border border-gray-800 shadow-xl p-4",
-                // light styles
-                "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-                // dark styles
-                "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
-                )}
-                >
-                <div className="flex flex-col  gap-2">
-                <Icons.Logo className="h-6 w-6 text-gray-800 dark:text-white" />
+          <FeatureGrid
+              title="Features"
+              subtitle="Unlock powerful capabilities for your project."
+              items={[
+                {
+                  icon: (
+                    <svg viewBox="0 0 24 24" className="h-12 w-12 fill-current">
+                      <path d="M11.572 0c-.176 0-.31.001-.358.007a19.76 19.76 0 0 1-.364.033C7.443.346 4.25 2.185 2.228 5.012a11.875 11.875 0 0 0-2.119 5.243c-.096.659-.108.854-.108 1.747s.012 1.089.108 1.748c.652 4.506 3.86 8.292 8.209 9.695.779.25 1.6.422 2.534.525.363.04 1.935.04 2.299 0 1.611-.178 2.977-.577 4.323-1.264.207-.106.247-.134.219-.158-.02-.013-.9-1.193-1.955-2.62l-1.919-2.592-2.404-3.558a338.739 338.739 0 0 0-2.422-3.556c-.009-.002-.018 1.579-.023 3.51-.007 3.38-.01 3.515-.052 3.595a.426.426 0 0 1-.206.214c-.075.037-.14.044-.495.044H7.81l-.108-.068a.438.438 0 0 1-.157-.171l-.05-.106.006-4.703.007-4.705.072-.092a.645.645 0 0 1 .174-.143c.096-.047.134-.051.54-.051.478 0 .558.018.682.154.035.038 1.337 1.999 2.895 4.361a10760.433 10760.433 0 0 0 4.735 7.17l1.9 2.879.096-.063a12.317 12.317 0 0 0 2.466-2.163 11.944 11.944 0 0 0 2.824-6.134c.096-.66.108-.854.108-1.748 0-.893-.012-1.088-.108-1.747-.652-4.506-3.859-8.292-8.208-9.695a12.597 12.597 0 0 0-2.499-.523A33.119 33.119 0 0 0 11.573 0zm4.069 7.217c.347 0 .408.005.486.047a.473.473 0 0 1 .237.277c.018.06.023 1.365.018 4.304l-.006 4.218-.744-1.14-.746-1.14v-3.066c0-1.982.01-3.097.023-3.15a.478.478 0 0 1 .233-.296c.096-.05.13-.054.5-.054z" />
+                    </svg>
+                  ),
+                  title: "Integration Middleware",
+                  description:
+                    "Our integration middleware allows you our customer to integrate your accounting system or any system to zimra for invoice processing. We can integrate to SAP B1, Sage, Xero, Pastel and Point of Sale",
+                },
+                {
+                  icon: (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 256 256"
+                      className="h-12 w-12 fill-current"
+                    >
+                      <rect width="256" height="256" fill="none"></rect>
+                      <line
+                        x1="208"
+                        y1="128"
+                        x2="128"
+                        y2="208"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        strokeWidth="22"
+                      ></line>
+                      <line
+                        x1="192"
+                        y1="40"
+                        x2="40"
+                        y2="192"
+                        fill="none"
+                        stroke="currentColor"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        strokeWidth="22"
+                      ></line>
+                    </svg>
+                  ),
+                  title: "Fiscalization as a service (FAAS)",
+                  description:
+                    "Our Web based fiscalization portal allows you to create invoices and submit invoices to Zimra without needing an accounting system.",
+                },
+                {
+                  icon: (
+                    <svg
+                      width="201"
+                      height="242"
+                      viewBox="0 0 201 242"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-12 w-12 fill-current"
+                    >
+                      <path d="M104.004 1.78785C101.751 0.662376 99.1002 0.663161 96.8483 1.78998L4.9201 47.7892C2.21103 49.1448 0.5 51.9143 0.5 54.9436V130.526C0.5 133.556 2.2123 136.327 4.92292 137.682L96.9204 183.67C99.1725 184.796 101.823 184.796 104.075 183.67L168.922 151.246C174.242 148.587 180.5 152.455 180.5 158.402V168.855C180.5 171.885 178.788 174.655 176.078 176.01L104.077 212.011C101.825 213.137 99.1745 213.137 96.9224 212.012L12.0771 169.598C6.75791 166.939 0.5 170.807 0.5 176.754V187.048C0.5 190.083 2.21689 192.856 4.93309 194.209L97.0051 240.072C99.2529 241.191 101.896 241.191 104.143 240.07L196.071 194.21C198.785 192.857 200.5 190.084 200.5 187.052V119.487C200.5 113.54 194.242 109.672 188.922 112.332L132.078 140.754C126.758 143.414 120.5 139.546 120.5 133.599V123.145C120.5 120.115 122.212 117.345 124.922 115.99L196.078 80.4124C198.788 79.0573 200.5 76.2872 200.5 73.257V54.9468C200.5 51.9158 198.787 49.1451 196.076 47.7904L104.004 1.78785Z" />
+                    </svg>
+                  ),
+                  title: "Mobile Fiscalization",
+                  description:
+                    "Our mobile based fiscalization application, allows you to fiscalize your invoices by creating them within the mobile app giving you convince .",
+                },
                 
-                <figcaption className="text-lg font-semibold dark:text-white">
-                  Integration middleware
-                </figcaption>
-                </div>
-                <p className="mt-2 text-sm">
-                Our integration middleware allows you our customer to integrate your accounting system or any system to zimra for invoice processing. We can integrate to SAP B1, Sage, Xero, Pastel and Point of Sale                </p>
-                </figure>
-              <figure
-                className={cn(
-                  "relative w-full max-w-xs cursor-pointer overflow-hidden rounded-2xl border border-gray-800 shadow-xl p-4",
-                  // light styles
-                  "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-                  // dark styles
-                  "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
-                )}
-              >
-                <div className="flex flex-col gap-2">
-                  <Icons.Logo className="h-6 w-6 text-gray-800 dark:text-white" />
-                  <figcaption className="text-lg font-semibold dark:text-white">
-                    Fiscalization as a service (FAAS)
-                  </figcaption>
-                </div>
-                <p className="mt-2 text-sm">
-                Our Web based fiscalization portal allows you to create invoices and submit invoices to Zimra without needing an accounting system. 
-                <br /> 
-                <br /> 
-                
-                </p>
-              </figure>
-
-              <figure
-                className={cn(
-                  "relative w-full max-w-xs cursor-pointer overflow-hidden rounded-2xl border border-gray-800 shadow-xl p-4",
-                  // light styles
-                  "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
-                  // dark styles
-                  "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
-                )}
-              >
-                <div className="flex flex-col gap-2">
-                  <Icons.Logo className="h-6 w-6 text-gray-800 dark:text-white" />
-                  <figcaption className="text-lg font-semibold dark:text-white">
-                  Mobile Fiscalization
-                  </figcaption>
-                </div>
-                <p className="mt-2  text-sm">
-                Our mobile based fiscalization application, allows you to fiscalize your invoices by creating them within the mobile app giving you convince .
-                <br />
-                <br />
-                </p>
-              </figure>
-              
-              
-            </div>
-
-
-
-
-          
+              ]}
+            />
         </div>
       </section>
 
---
+++ components/features.tsx
@@ -0,0 +1,40 @@
+export function FeatureGridItem(props: {
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+}) {
+  return (
+    <div className="relative overflow-hidden rounded-lg border bg-background p-2">
+      <div className="flex h-[250px] flex-col rounded-md p-6 gap-4">
+        {props.icon}
+        <div className="space-y-2">
+          <h3 className="font-bold">{props.title}</h3>
+          <p className="text-sm text-muted-foreground">{props.description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function FeatureGrid(props: {
+  title: string;
+  subtitle: string;
+  items: {
+    icon: React.ReactNode;
+    title: string;
+    description: string;
+  }[];
+}) {
+  return (
+    <section
+      id="features"
+      className="container space-y-6 py-8 md:py-12 lg:py-24"
+    >
+      <div className="mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-5xl md:grid-cols-3">
+        {props.items.map((item, index) => (
+          <FeatureGridItem key={index} {...item} />
+        ))}
+      </div>
+    </section>
+  );
+}
